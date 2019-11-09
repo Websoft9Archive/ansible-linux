d8752b1db6759a45e60f1f02e0143f31e65b2a4c
hliblaoutlook.comck <hliblack@>
2019-11-09 15:55:44 +0800

    .

--- ansible_script/functions.py
+++ ansible_script/functions.py
@@ -8,8 +8,12 @@ def wirte_file_local(hosts):
     with open(hosts, 'r') as hosts:
         hosts.write("[local] \n")
         hosts.write("localhost")
+        hosts.close()
 
 def write_file_remote(hosts, ip, username, password):
+    os.system("sed -i 's/#host_key_checking = False/host_key_checking = False/g'  /etc/ansible/ansible.cfg")
     with open(hosts, 'w') as hosts:
         hosts.write("[remote] \n")
         hosts.write(ip + "\t ansible_ssh_user=" + username + "\t ansible_ssh_pass=" + password + "\t ansible_sudo_pass=" + password)
+        hosts.closed
+
