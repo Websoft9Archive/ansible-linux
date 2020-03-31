4c3652cffcfc94cfcd2407e2ff27d99aab2544c3
chendelin1982 <chendelin1982@126.com>
2020-03-31 14:11:57 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,7 +43,7 @@ while :; do
   esac
 done
 
-echo "Pre-installation starting, please wait for 1-3 minute "
+echo "Pre-installation starting, please wait for 1-3 minutes..."
 
 # install pip for Oracle which yum have not pip
 q_str=$(lsb_release -a);s_str="Oracle"; if [[ $q_str == *$s_str* ]];then curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py;sudo python get-pip.py;fi
