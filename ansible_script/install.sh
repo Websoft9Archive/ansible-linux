a0d7f4bccc9ddff0584388390c058b536c03c100
chendelin1982 <chendelin1982@126.com>
2020-03-31 12:31:20 +0800

    Update install.sh

--- ansible_script/install.sh
+++ ansible_script/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-clear
 
 # Check if user is root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script, please use 'sudo su -' command to change root"; exit 1; }
