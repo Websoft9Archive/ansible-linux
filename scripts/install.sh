e1804212f1085dc25465fd76eb6634a7a65c9d4b
chendelin1982 <chendelin1982@126.com>
2020-04-02 16:24:41 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,8 +54,6 @@ if [[ $repo_name != "" ]]
 then
 pip3 install -U --force-reinstall ansible
 rm -rf  /tmp/ansible-$repo_name
-git config --global user.name "hliblack"
-git config --global user.email "hliblack@outlook.com"
 cd /tmp; git clone https://github.com/Websoft9/ansible-$repo_name.git;
 echo "localhost" > /tmp/ansible-$repo_name/hosts
 cd ansible-$repo_name;ansible-galaxy install -r requirements.yml -f
